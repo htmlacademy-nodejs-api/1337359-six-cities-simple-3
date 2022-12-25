1675d06ea07f6b0045af0f23e5461a7885a4b546
Nataly <nataly-wien@yandex.ru>
2022-12-25 19:36:37 +0300

    Вносит небольшое исправление

--- src/cli-command/version-command.ts
+++ src/cli-command/version-command.ts
@@ -1,5 +1,6 @@
+import chalk from 'chalk';
 import { readFileSync } from 'fs';
-import { CliCommandInterface } from './cli-command.interface.js'
+import { CliCommandInterface } from './cli-command.interface.js';
 
 export default class VersionCommand implements CliCommandInterface {
   public readonly name = '--version';
@@ -13,6 +14,6 @@ export default class VersionCommand implements CliCommandInterface {
 
   public async execute(): Promise<void> {
     const version = this.readVersion();
-    console.log(version);
+    console.log(chalk.yellowBright(version));
   }
 }
